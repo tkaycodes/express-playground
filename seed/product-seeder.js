31a3d44f68f4e00e1c41b98c6cd019c6b239b37a
Tabish Khan <tabish.khan@widerfunnel.com>
2018-08-28 17:20:42 -0700

    Add faker dependency, render faker data, random images, add price and add to cart cta for each product

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -1,31 +1,33 @@
 var Product = require('../models/product');
 var mongoose = require('mongoose');
+var faker = require('faker');
+var products = [];
+var done = 0;
+var images = [
+	"http://cdn.shopify.com/s/files/1/0278/2953/articles/Blog2_600x.jpg?v=1495210341",
+	"https://www.kiwicamping.co.nz/Images/Categories/pack.jpg",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJxLQsSbH1_uqSO6w89uQGsewl-UaL-dlfoLMS3i_eNHAAOJId",
+	"https://cdn.shopify.com/s/files/1/0817/7355/products/71oLXNG1OjL._SL1500_large.jpg?v=1490047907",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCFILxIfhYJYPf-86a_fsMAUNkzxIjc7xUOHK3XcjVGnO5040WAw",
+	"http://www.langya-camping.com/Uploads/5af95bc12607d.png",
+	"https://images-na.ssl-images-amazon.com/images/I/31GO3dFTgDL._US500_.jpg"
+]
 
 mongoose.connect('mongodb://localhost:27017/store', {useNewUrlParser: true});
 
-var products = [
-	new Product({
-		imagePath: 'https://www.kiwicamping.co.nz/Images/Categories/pack.jpg',
-		title: 'cool bag',
-		description: 	'This is a cool bag with lots of features!!',
-		price: 			400
-	}),
-	new Product({
-		imagePath: 'https://www.kiwicamping.co.nz/Images/Categories/pack.jpg',
-		title: 'cool bag 2',
-		description: 'This is another cool bag with lots of features!!',
-		price: 600
-	}),
-	new Product({
-		imagePath: 'https://www.kiwicamping.co.nz/Images/Categories/pack.jpg',
-		title: 'cool bag 3',
-		description: 'This is a third cool bag with lots of features!!',
-		price: 900
-	})
-]
+// PUSH ITEMS TO ARRAY
+for (var i = 0; i < 10; i++) {
+	products.push(
+		new Product({
+			title: faker.commerce.productName(),
+			description: faker.lorem.paragraph(),
+			imagePath: images[Math.floor(Math.random() * images.length)],
+			price: faker.finance.amount()
+		})
+	)
+}
 
-var done = 0;
-
+// WAIT UNTIL ALL PRODUCTS SEEDED
 for (var i = 0; i< products.length; i++) {
 	products[i].save(function() {
 		done++;
